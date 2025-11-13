b0e3da1c07c917264887ab406945144493a61785
cryptocoledotcom <colebowersock@gmail.com>
2025-11-13 13:35:18 -0500

    removed tailwind

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,29 +15,31 @@ function Navbar() {
         <img src={logo} alt="Fastrack Driving School" />
       </Link>
       <ul className="nav-menu">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/courses" className="nav-link">Courses</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">About Us</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">Contact</Link>
-        </li>
         {user ? (
+          // --- Logged-in User View ---
           <>
             <li className="nav-item">
-              <Link to="/user-profile" className="nav-link">Profile</Link>
+              <Link to="/user-profile" className="nav-link">My Profile</Link>
             </li>
             <li className="nav-item">
               <button onClick={logout} className="nav-link logout-button">Logout</button>
             </li>
           </>
         ) : (
+          // --- Visitor View ---
           <>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/courses" className="nav-link">Courses</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About Us</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
             <li className="nav-item">
               <Link to="/login" className="nav-link login-link">Login</Link>
             </li>
